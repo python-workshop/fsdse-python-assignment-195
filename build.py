8cdcc1cd034730e135fa650d3b0e1f85614de8b3
sangam <sangam.angre@gmail.com>
2017-05-23 11:57:30 +0530

    test cases created

--- build.py
+++
@@ -1,9 +0,0 @@
-#Check if a number is prime
-def is_prime(num):
-    if num == 0 or num == 1:
-        return False
-    for x in range(2, num):
-        if num % x == 0:
-            return False
-    else:
-        return True
